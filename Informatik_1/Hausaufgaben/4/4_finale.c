1689240247616db796d526cc7422720f75549b2a
HelyusHD <you@example.com>
2024-11-11 19:31:03 +0100

    added heatmap changed size def of map

--- Informatik_1/Hausaufgaben/4/4_finale.c
+++ Informatik_1/Hausaufgaben/4/4_finale.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <conio.h>
 
-#define KARTENBREITE 100  // Breite der Karte also x
-#define KARTENHÖHE 100    // Höhe der Karte
-#define SCHRITTGRÖSSE 50  // Iterationsschrittgröße der Abtastung
+#define SCHRITTGROESSE 10  // Iterationsschrittgröße der Abtastung
+#define KARTENSCHRITTBREITE 10  // Breite der Karte in Schritten also x
+#define KARTENSCHRITTHOEHE 10    // Höhe der Karte in Schritten
+
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
 
 typedef struct {
     int x;
@@ -34,30 +37,45 @@ int compare_distance(const void *a, const void *b) {
 
 int main() {
     Vector2 haeuser[] = {
+        { .x =  0, .y =  0 },
         { .x = 10, .y = 10 },
-        { .x = 20, .y = 10 },
-        { .x = 30, .y = 10 },
-        { .x = 40, .y = 10 },
-        { .x = 50, .y = 10 }
+        { .x = 20, .y = 20 },
+        { .x = 30, .y = 30 }
     };
     int num_haeuser = sizeof(haeuser) / sizeof(haeuser[0]);
+    int heatMap[KARTENSCHRITTBREITE+1][KARTENSCHRITTHOEHE+1];
 
-    // Iteriere über die Karte in Schritten von SCHRITTGRÖSSE
-    for (pos.x = 0; pos.x <= KARTENBREITE; pos.x += SCHRITTGRÖSSE) {
-        for (pos.y = 0; pos.y <= KARTENHÖHE; pos.y += SCHRITTGRÖSSE) {
-
+    // Iteriere über die Karte in Schritten von SCHRITTGROESSE
+    for (int xStep = 0; xStep <= KARTENSCHRITTBREITE; xStep++) {
+        pos.x = xStep * SCHRITTGROESSE;
+        for (int yStep = 0; yStep <= KARTENSCHRITTHOEHE; yStep++) {
+            pos.y = yStep * SCHRITTGROESSE;
             // Sortiere die Häuser basierend auf der Distanz zur aktuellen Position `pos`
             qsort(haeuser, num_haeuser, sizeof(Vector2), compare_distance);
-
-            // Ausgabe der sortierten Häuser
-            printf("Sortierte Häuser von Position (%d, %d):\n", pos.x, pos.y);
-            for (int i = 0; i < num_haeuser; i++) {
-                printf("Haus %d: x = %d, y = %d, Distanz = %.2f\n",
-                       i, haeuser[i].x, haeuser[i].y, distance(pos, haeuser[i]));
-            }
-            printf("\n");
+            heatMap[xStep][yStep] = MIN(
+                (int)floor(
+                        distance(pos, haeuser[0]) / 10
+                ),9
+            );
         }
     }
+    for (int xStep = 0; xStep <= KARTENSCHRITTBREITE; xStep++) {
+        for (int yStep = 0; yStep <= KARTENSCHRITTHOEHE; yStep++) {
+            (heatMap[xStep][yStep] <= 9 ?  printf("%i ",heatMap[xStep][yStep]) :  printf("# "));
+        }
+        printf("\n");
+    }
+
+    char ch;
+    printf("Druecke 'q' zum Beenden.\n");
+    // Endlosschleife, bis 'q' gedrückt wird
+    while (1) {
+        ch = getch(); // Wartet auf einen Tastendruck
+        if (ch == 'q' || ch == 'Q') {
+            break; // Schleife beenden, wenn 'q' oder 'Q' gedrückt wurde
+        }
+    }
+    printf("Programm beendet.\n");
 
     return 0;
 }
