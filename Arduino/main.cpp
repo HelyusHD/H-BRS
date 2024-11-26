d26fc6ef46798babbdb5251ab64a7f9b7d69e238
HelyusHD <you@example.com>
2024-11-26 01:35:54 +0100

    added IR remote control

--- Arduino/main.cpp
+++ Arduino/main.cpp
@@ -3,15 +3,29 @@
 #include <math.h>
 #include <IRremote.h>
 
+#define BUTTON0 3910598400
+#define BUTTON1 4077715200
+#define BUTTON2 3877175040
+#define BUTTON3 2707357440
+#define BUTTON4 4144561920
+#define BUTTON5 3810328320
+#define BUTTON6 2774204160
+#define BUTTON7 3175284480
+#define BUTTON8 2907897600
+#define BUTTON9 3041591040
+#define BUTTONON 3125149440
+
+#define RECIVER_PIN 4
 #define LED_PIN 5    // Replace with the pin number you used for DATA connection
+
 #define NUM_LEDS 47  // Replace with the number of LEDs in your strip
-#define DELAY_TIME 10  // Delay between LED movements (in milliseconds)
+#define DELAY_TIME 100  // Delay between LED movements (in milliseconds)
 CRGB leds[NUM_LEDS];
 
 const int RECV_PIN = 7;
-IRrecv irrecv(RECV_PIN);
-decode_results results;
-
+bool on;
+int r, g, b;
+int t = 0, max_t = NUM_LEDS*10;
 
 // Funktion zum Umwandeln von HSV in RGB
 void hsv_to_rgb(float h, float s, float v, int *r, int *g, int *b) {
@@ -40,7 +54,6 @@ void hsv_to_rgb(float h, float s, float v, int *r, int *g, int *b) {
   *b = (int)((b_prime + m) * 255);
 }
 
-
 // Hauptfunktion für den Regenbogen
 void rainbow_color(int t, int max_t, int *r, int *g, int *b) {
     if (t < 0) t = 0;
@@ -50,39 +63,52 @@ void rainbow_color(int t, int max_t, int *r, int *g, int *b) {
     hsv_to_rgb(hue, 1.0, 1.0, r, g, b); // Volle Sättigung und Helligkeit
 }
 
-int r, g, b;
-int t = 0, max_t = 1000;
-
-void setup() {
-  FastLED.addLeds<WS2811, LED_PIN, RGB>(leds, NUM_LEDS);
-  Serial.begin(9600);
-  irrecv.enableIRIn();
-  Serial.println("IR Receiver is ready");
+void setup()
+{
+    Serial.begin(9600);
+    IrReceiver.begin(RECIVER_PIN, ENABLE_LED_FEEDBACK);
+    Serial.print("running\n");
+    on = true;
+    FastLED.addLeds<WS2815, LED_PIN, RGB>(leds, NUM_LEDS);
 }
 
-
-
 void loop() {
-  if (irrecv.decode(&results)){
-    //Serial.println(results.value, HEX);
-    Serial.print("Signal empfangen: ");
-    Serial.println(results.value, HEX); // Ausgabe des Signals im Hexadezimalformat
-    irrecv.resume(); // Nächstes Signal verarbeiten
-  }
+    if (IrReceiver.decode()) {
+        uint32_t rawData= IrReceiver.decodedIRData.decodedRawData;
+        Serial.print(rawData);
+        Serial.print("\n");
 
-  for (int i = 0; i < NUM_LEDS; i++) {
-    // int p = t + i*(int)floor(max_t / NUM_LEDS);
-    int p = t + i;
-    if (p > max_t) p = p - max_t;
-    rainbow_color(p, max_t, &r, &g, &b);
-    leds[i].r = r;
-    leds[i].g = g; 
-    leds[i].b = b;
-  }
-  t += 1;
-  if (t > max_t) t = 0;
-  FastLED.show();  // Update the LED strip
-  delay(DELAY_TIME);  // Delay between LED movements
-  //Serial.println(t);
+        if(rawData == BUTTONON){
+            Serial.print("pressed ON\n");
+            if(on){
+                on = false;
+                for (int i = 0; i < NUM_LEDS; i++) {
+                    leds[i].r = 0;
+                    leds[i].g = 0; 
+                    leds[i].b = 0;
+                }
+                FastLED.show();  // Update the LED strip
+            }else{
+                on = true;
+            }
+        }else if(rawData == BUTTON1) {
+            Serial.print("pressed 1\n");
+        }
+        IrReceiver.resume(); 
+    }else if (on){
+    for (int i = 0; i < NUM_LEDS; i++) {
+        // int p = t + i*(int)floor(max_t / NUM_LEDS);
+        int p = t + i;
+        if (p > max_t) p = p - max_t;
+        rainbow_color(p, max_t, &r, &g, &b);
+        leds[i].r = r;
+        leds[i].g = g; 
+        leds[i].b = b;
+    }
+    t += 5;
+    if (t > max_t) t = 0;
+    FastLED.show();  // Update the LED strip
+    delay(DELAY_TIME);  // Delay between LED movements
+    }
 
 }
